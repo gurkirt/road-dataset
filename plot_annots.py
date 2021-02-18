aaca5441375a17f00abc5d6580ad63cf3808a9c3
Gurkirt Singh <guru094@gmail.com>
2021-02-18 17:17:25 +0100

    u

---
+++ plot_annots.py
@@ -0,0 +1,78 @@
+import json, pdb, argparse
+import cv2, os
+base_dir = './'
+
+FONT = cv2.FONT_HERSHEY_SIMPLEX
+
+SAVE = True
+
+
+def main(video_name, input_images_dir, output_images_dir):
+
+    
+    video_plot_dir = os.path.join(output_images_dir, video_name)
+    
+    # vidfile = video_name[:-4]
+    
+    if not os.path.isdir(video_plot_dir):
+        os.makedirs(video_plot_dir)
+    
+    # video_handle = cv2.VideoCapture(vidfile)
+    video_writter_object =  cv2.VideoWriter()
+
+    with open(annofile,'r') as f:
+        db = json.load(f)
+
+    # pdb.set_trace()
+    color = (0,0,255)
+    color1 = (255,0,5)
+
+    frames = db['frames']
+
+    for f in sorted(frames.keys()):
+        print(f)
+        in_id = int(f)
+        # input_image_path = '{:s}/{:05d}.png'.format(imagedir, 3*(in_id-1)+1)
+        # import cv2
+
+        color = (0, 0, 255)
+        color1 = (255, 0, 5)
+
+        flag, frame = video_handle.retrive()
+        
+        for i in range(3):
+            _, _ = video_handle.retrive()
+
+        annots = frames[f]
+        for anno in annots:
+            width = anno['width']
+            height = anno['height']
+            box = anno['box']
+            tags = anno['tags']
+
+            pt = [int(box['x1']), int(box['y1']),int(box['x2']),int(box['y2'])]
+            cv2.rectangle(frame, (pt[0], pt[1]), (pt[2], pt[3]), color, 2)
+            offset = 0
+            for tag in tags:
+                cv2.putText(frame, tag, (int(pt[0]) , int(pt[1]) + offset), FONT, 1, color, 1, cv2.LINE_AA)
+                offset += 21
+        out_image = '{:s}/{:05d}.png'.format(outdir, in_id)
+        cv2.imwrite(out_image,frame)
+
+
+if __name__ == '__main__':
+    p = argparse.ArgumentParser(description='extract frame from videos')
+    p.add_argument('data_dir', type=str,
+                   help='Video directory where videos are saved.')
+    args = p.parse_args()
+    input_images_dir = os.path.join(args.data_dir, 'rgb-images')
+    video_dirs = os.listdir(input_images_dir)
+    video_dirs = [af for af in video_dirs if len(af)>3]
+    output_images_dir = os.path.join(args.data_dir, 'plotted-images')
+    print('NUMBER OF VIDEO FILES are:::>', len(video_dirs))
+
+
+    for video_name in video_dirs:
+            print('\n\n\n\n\n annofile ', video_name, '\n\n\n\n\n\n')
+            main(video_name, input_images_dir, output_images_dir)
+
